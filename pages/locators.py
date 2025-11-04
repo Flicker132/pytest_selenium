f3a92b2afd37d088316a6bca7cdf96c4b912c3e4
Flicker132 <zhigalov.k.o@gmail.com>
2025-11-04 18:05:51 +0500

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,18 +1,8 @@
 from selenium.webdriver.common.by import By
 
-
-class BasePageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
-
-
-class MainPageLocators:
-    pass
-
-
 class ProductPageLocators:
-    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
-    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
-    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
-    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child .alertinner strong")
-    BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "div.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "p.price_color")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "div.alert-success:nth-child(1) .alertinner strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, "div.alert-info .alertinner strong")
