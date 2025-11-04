35508920d09b5b722b3d3263d4ce4ae348eeb52b
Flicker132 <zhigalov.k.o@gmail.com>
2025-11-04 18:02:03 +0500

    Create product_page.py

---
+++ pages/product_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        add_button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
+        add_button.click()
+
+    def should_be_product_added_to_basket_message(self):
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        message_product_name = self.browser.find_element(*ProductPageLocators.SUCCESS_MESSAGE).text
+        assert product_name == message_product_name, \
+            f"Product name in message doesn't match. Expected: '{product_name}', got: '{message_product_name}'"
+
+    def should_be_basket_total_message(self):
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        basket_total = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL).text
+        assert product_price == basket_total, \
+            f"Basket total doesn't match product price. Expected: '{product_price}', got: '{basket_total}'"
