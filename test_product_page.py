79671ac239c74cbe0b24aa021561ef030ec72c64
Flicker132 <zhigalov.k.o@gmail.com>
2025-11-04 18:11:18 +0500

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -2,8 +2,12 @@ import pytest
 from pages.product_page import ProductPage
 
 
-@pytest.mark.parametrize('offer', ["offer0", "offer1", "offer2", "offer3", "offer4",
-                                   "offer5", "offer6", "offer7", "offer8", "offer9"])
+@pytest.mark.parametrize('offer', [
+    "offer0", "offer1", "offer2", "offer3", "offer4",
+    "offer5", "offer6",
+    pytest.param("offer7", marks=pytest.mark.xfail(reason="Known bug in offer7")),
+    "offer8", "offer9"
+])
 def test_guest_can_add_product_to_basket(browser, offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={offer}"
     page = ProductPage(browser, link)
