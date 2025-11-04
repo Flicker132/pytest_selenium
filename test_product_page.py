85a4be7e8b17556a25b147a9f99fdddccdd69f14
Flicker132 <zhigalov.k.o@gmail.com>
2025-11-04 18:08:55 +0500

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,13 +1,11 @@
 import pytest
 from pages.product_page import ProductPage
 
-links = [
-    "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear",
-    "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-]
 
-@pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser, link):
+@pytest.mark.parametrize('offer', ["offer0", "offer1", "offer2", "offer3", "offer4",
+                                   "offer5", "offer6", "offer7", "offer8", "offer9"])
+def test_guest_can_add_product_to_basket(browser, offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo={offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
